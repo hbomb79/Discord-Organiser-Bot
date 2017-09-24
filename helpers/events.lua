24e03943920c8e7faac673ec87651772a7110f91
hbomb79 <harryfelton12@gmail.com>
2017-09-24 22:49:28 +1300

    Removed useless local definition

--- helpers/events.lua
+++ helpers/events.lua
@@ -127,7 +127,7 @@ events.pushEvent = function( self, user, target )
 	local event = self:getEvent { id = user }
 
 	if target == CHANNEL then target:bulkDelete() end
-	local r = reporter:info(
+	reporter:info(
 		target, event.title, event.desc,
 		{ name = "Location", value = event.location, inline = true },
 		{ name = "Timeframe", value = event.timeframe, inline = true },
