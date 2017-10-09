ba852dcc643fa519f486791a1da3171741c6a36e
hbomb79 <harryfelton12@gmail.com>
2017-10-10 12:45:54 +1300

    Added settings command

--- src/lib/commands.lua
+++ src/lib/commands.lua
@@ -14,6 +14,10 @@ local function splitArguments( val )
     return parts
 end
 
+local VALID_SETTINGS = {
+    prefix = "The character(s) that must prefix a bot command. Can be set to any alphanumeric value between 1-10 characters in length (Default: !)",
+    channelID = "The ID of the channel that is used for event information. (Default: the first channel the bot finds upon joining)"
+}
 --[[
     WIP
 ]]
@@ -24,10 +28,10 @@ Logger.d "Building commands list (commands.lua)"
 local commands
 commands = {
     help = {
-        help = "Display this help menu.\n\nRun **!help commands [command1, command2, ...]** to see help on the given commands (eg: **!help commands create cancel** gives help for the *create* and *cancel* command).",
+        help = "Display this help menu.\n\nRun 'cmd help commands [command1, command2, ...]' to see help on the given commands (eg: 'cmd help commands create cancel' gives help for the *create* and *cancel* command).",
         action = function( evManager, guildID, userID, message )
             local user = evManager.worker.client:getUser( userID )
-            local args = splitArguments( message.content:match "^%!%w+%s*(.*)$" )
+            local args = splitArguments( select( 2, evManager.worker.commandManager:splitCommand( guildID, message.content ) ) )
             if args[ 1 ] == "commands" then
                 if args[ 2 ] then
                     Logger.i "Serving help information for requested commands"
@@ -39,7 +43,7 @@ commands = {
                             Logger.d( "Serving help for cmd " .. com )
 
                             if h and #h > 0 then
-                                fields[ #fields + 1 ] = { name = "__!".. com .. "__", value = h }
+                                fields[ #fields + 1 ] = { name = "'cmd ".. com .. "'", value = h }
                             end
                         else
                             Logger.w( "Help information not available for "..com )
@@ -64,7 +68,7 @@ commands = {
                     local fields = {}
                     for name, config in pairs( commands ) do
                         if config.help and #config.help > 0 then
-                            fields[ #fields + 1 ] = { name = "__!" .. name .. "__", value = config.help }
+                            fields[ #fields + 1 ] = { name = "'cmd " .. name .. "'", value = config.help }
                         end
                     end
 
@@ -73,13 +77,48 @@ commands = {
                 end
             else
                 Reporter.info( user, "Hoorah! You're ready to learn a little bit about meee!", "Using this bot is dead simple! Before we get going select what information you need\n\n",
-                    { name = "Hosting", value = "To host an event use the **!create** command inside this DM.\nOnce created you will be sent more instructions here (regarding configuration and publishing of your event)" },
-                    { name = "Responding", value = "If you want to let the host of an event know whether or not you're coming, use **!yes**, **!no** or **!maybe** inside this DM. The event manager will be notified." },
+                    { name = "Hosting", value = "To host an event use the 'cmd create' command inside this DM.\nOnce created you will be sent more instructions here (regarding configuration and publishing of your event)" },
+                    { name = "Responding", value = "If you want to let the host of an event know whether or not you're coming, use 'cmd yes', 'cmd no' or 'cmd maybe' inside this DM. The event manager will be notified." },
                     { name = "Current Event", value = "If you'd like to see information regarding the current event visit the $HOST_CHANNEL inside the $GUILD (BG'n'S server)." },
-                    { name = "Further Commands", value = "Use **!help commands** to see information on all commands.\n\nIf you're only interested in certain commands, provide the names of the commands as well to see information for those commands only (eg: !help commands create)."}
+                    { name = "Further Commands", value = "Use 'cmd help commands' to see information on all commands.\n\nIf you're only interested in certain commands, provide the names of the commands as well to see information for those commands only (eg: !help commands create)."}
                 )
 
                 return Logger.s "Served general help information"
+            end
+        end
+    },
+
+    settings = {
+        action = function( eventManager, guildID, userID, message )
+            local args = splitArguments( select( 2, eventManager.worker.commandManager:splitCommand( guildID, message.content ) ) )
+            local guildConfig = eventManager.worker.guilds[ guildID ]
+
+            local settingArg = args[ 1 ]
+            if not settingArg then
+                local fields = {}
+                for name, help in pairs( VALID_SETTINGS ) do
+                    fields[ #fields + 1 ] = { name = ("__%s__"):format( name ), value = help }
+                end
+
+                Reporter.info( message.channel, "Guild Settings", "This command can be used to configure the bot specifically for your guild. Use 'cmd setting command [value]', where `command` is one of below. If 'value' is given, the setting will be set to that value (unless value is 'nil', in which case the setting will be reset) -- otherwise the current value will be shown.", unpack( fields ) )
+                return Logger.s( "Served general settings information to user " .. userID .." via guild " .. guildID .. " channel " .. message.channel.id .. " ("..message.channel.name..")" )
+            end
+
+            local isGetting = not ( args[ 2 ] and #args[ 2 ] > 0 )
+            if VALID_SETTINGS[ settingArg ] then
+                if isGetting then
+                    Reporter.info( message.channel, "Guild setting '"..settingArg.."'", "The value for this guild setting is '"..tostring( guildConfig[ settingArg ] ).."'" )
+                    return Logger.s( "Served guild setting '"..settingArg.."' for guild '"..guildID.."'" )
+                else
+                    local targetVal = args[ 2 ] ~= "nil" and args[ 2 ] or nil
+                    guildConfig[ settingArg ] = targetVal
+                    Reporter.success( message.channel, "Updated guild setting", targetVal and "The value of the guild setting '"..settingArg.."' has been changed to '"..tostring( targetVal ).."'" or "Guild setting '"..settingArg.."' has been unset" )
+
+                    eventManager.worker:saveGuilds()
+                    return Logger.s( "Updated guild setting '"..settingArg.."' to '"..args[ 2 ].."' under instruction from user '"..userID.."' at guild '"..guildID.."'" )
+                end
+            else
+                Reporter.failure( message.channel, "Unknown guild setting", "The guild setting '"..settingArg.."' doesn't exist, ensure you haven't made a typing mistake. Check 'cmd settings' for a list of valid settings" )
             end
         end
     },
